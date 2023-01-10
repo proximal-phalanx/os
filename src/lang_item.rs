905f7896db7a07895752e8770324598584ee893f
Zend <zendwang040302@outlook.com>
2023-01-10 20:40:29 +0800

    test

--- src/lang_item.rs
+++ src/lang_item.rs
@@ -1,9 +1,7 @@
 /// 这个函数将在 panic 时被调用
 
 // suppress error
-#[cfg(not(test))]
 use core::panic::PanicInfo;
-#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     use crate::println;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 mod lang_item;
 mod vga_buffer;
 
 #[no_mangle]
-pub extern "C" fn _start() -> !{
-    println!("Hello World{}", "!");
+pub extern "C" fn _start() -> ! {
+    println!("[Kernel] Hello world!");
+    #[cfg(test)]
+    test_main();
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[no_mangle]
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
